389ce0aef6c4dc0987e54b7c9ca21bf23752f596
IamdebashisD <adebashisdas626@gmail.com>
2025-10-14 17:12:28 +0530

    Add User model structure and database schema

---
+++ app/models/user_model.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+import uuid
+import bcrypt
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users" # Table name in database
+    
+    id: str = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4())) # Using UUID as primary key
+    username: str = Column(String(50), nullable=False, unique=True)
+    email: str = Column(String(100), nullable=False, unique=True)
+    _password: str = Column("password", String(255), nullable=False)
+    created_at: datetime.datetime = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    
+    def __init__(self, username: str, email: str, password: str)-> None:
+        self.username = username
+        self.email = email
+        self.password = password
+    
+    
+    # Password property: Getter
+    @property
+    def password(self) -> str:
+        return self._password
+    
+    @password.setter
+    def password(self, plain_text: str) -> None:
+        self._password = bcrypt.hashpw(
+            plain_text.encode('utf-8'), bcrypt.gensalt()
+        ).decode('utf-8')
+        
+    def verify_password(self, plain_text: str)-> None:
+        return bcrypt.checkpw(
+            plain_text.encode('utf-8'), self._password.encode('utf-8')
+        )  
+    
+    def __repr__(self) -> str:
+        return f"<User(id={self.id}, username={self.username}, email={self.email})>"
+        
