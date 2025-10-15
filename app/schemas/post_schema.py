f7e045fd5206102b3e2aeb083669360a37f3bdbc
IamdebashisD <adebashisdas626@gmail.com>
2025-10-16 02:54:07 +0530

    Add PostSchema for model serialization

---
+++ app/schemas/post_schema.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema, fields, validate
+
+class PostSchema(Schema):
+    '''
+    Schema for validating and serializing Post data.
+    '''
+    id = fields.Str(dump_only=True)
+    content = fields.Str(
+        required=True,
+        validate=validate.Length(min=1, max=2000),
+        error_messages={
+            "required": "Content is required.",
+            "null": "Content cannot be null.", 
+        }
+    )
+    user_id = fields.Str(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
