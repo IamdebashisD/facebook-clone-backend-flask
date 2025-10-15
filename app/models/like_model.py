dccbf4c5d8b889f7eb79e7cbbb35a4b9e6fd7a6f
IamdebashisD <adebashisdas626@gmail.com>
2025-10-16 02:50:03 +0530

    removed one unwanted import satement in like model file

--- app/models/like_model.py
+++ app/models/like_model.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
 from app.database.db import Base
 import uuid
 import datetime
@@ -9,7 +8,7 @@ class Like(Base):
     
     id: str = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     post_id: str = Column(String(36), ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
-    user_id: str = Column(String(36), ForeignKey("users.id", ondelete="CASECADE"), nullable=False)
+    user_id: str = Column(String(36), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     created_at: datetime.datetime = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
     
     def __repr__(self) -> str:
