9f6cd6f50b0a3799484481fe1f946999bfe648c4
IamdebashisD <adebashisdas626@gmail.com>
2025-10-19 01:14:06 +0530

    update the "jwt_helper.py" file

--- app/utils/jwt_helper.py
+++ app/utils/jwt_helper.py
@@ -1,7 +1,7 @@
 import jwt
 from dotenv import load_dotenv
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone      # Import timezone
 from typing import Dict, Any
 
 load_dotenv() # Load environment variables from .env file
@@ -9,21 +9,26 @@ load_dotenv() # Load environment variables from .env file
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 ALGORITHM: str = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15)
-REFRESH_TOKEN_EXPIRE_DAYS = os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7)
+
+# 1: Cast environment variables to int() here. 
+# They are strings when retrieved from os.getenv(), which causes the TypeError in timedelta.
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 
 
 def create_access_token(data: Dict):
     payload: Dict = data.copy()
-    expire = datetime.utcnow + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    payload.update({"exp": expire, "type": "access"})
+    now = datetime.now(timezone.utc)
+    expire = now + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload.update({"exp": expire.timestamp(), "type": "access"})
     encoden_jwt = jwt.encode(payload, SECRET_KEY, ALGORITHM)
     return encoden_jwt
 
 def create_refresh_token(data: Dict):
     payload: Dict = data.copy()
-    expire = datetime.utcnow + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
-    payload.update({"exp": expire, "type": "refresh"})
+    now = datetime.now(timezone.utc)
+    expire = now + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    payload.update({"exp": expire.timestamp(), "type": "refresh"})
     encoden_jwt = jwt.encode(payload, SECRET_KEY, ALGORITHM)
     return encoden_jwt
 
