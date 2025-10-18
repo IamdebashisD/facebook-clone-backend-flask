2f3afe3d8d619a5ba39170bf501a6d11f77ca19d
IamdebashisD <adebashisdas626@gmail.com>
2025-10-19 01:10:16 +0530

    import all your models here to ensure sqlalchemy is aware of them

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,15 @@ from flask import Flask
 from .database.db import Base, engine
 from app.routes import all_blueprints
 
+
+# --- FIX START ---
+# IMPORT ALL YOUR MODELS HERE to ensure SQLAlchemy is aware of them 
+# before calling create_all(). This resolves the "failed to locate a name ('Post')" error.
+import app.models.user_model
+import app.models.post_model
+import app.models.like_model
+import app.models.comment_model
+
 def create_app():
     app = Flask(__name__)
     
