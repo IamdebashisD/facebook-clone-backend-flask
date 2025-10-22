c8eeabbd10c9d1b60383bf2c902343ef4cbd944b
IamdebashisD <adebashisdas626@gmail.com>
2025-10-22 19:38:02 +0530

    refactor(routes): Update Blueprint loading in __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,7 @@
 from .ping_routes import ping_bp
 from .auth_routes import auth_bp
 from .user_routes import user_bp
+from .post_routes import post_bp
 
-all_blueprints = [ping_bp, auth_bp, user_bp]
+all_blueprints = [ping_bp, auth_bp, user_bp, post_bp]
 
