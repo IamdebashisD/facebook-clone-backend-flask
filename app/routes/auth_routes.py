359be1c22870e381810878d2a993b9e17acdfc9a
IamdebashisD <adebashisdas626@gmail.com>
2025-10-28 19:13:12 +0530

    feat(auth): Implement logout route and blacklist token on logout

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, request, jsonify, Response
+from flask import Blueprint, request, jsonify, Response, g
 from sqlalchemy import or_
 from sqlalchemy.orm import Session, Query
 from app.database.db import SessionLocal
 from app.models.user_model import User
 from app.schemas.user_schema import UserSchema
 from typing import Dict, Any
+from app.utils.jwt_helper import create_access_token, create_refresh_token, decode_token
+from app.utils.token_required import token_required
+from app.models.token_blacklist_model import TokenBlacklist
+import datetime
+from app.utils.response_helper import api_response
 
 
 '''create Blueprint'''
@@ -62,7 +67,6 @@ def register() -> Response:
         
         
 '''<------  Login Route  ------>'''
-from app.utils.jwt_helper import create_access_token, create_refresh_token, decode_token
 
 @auth_bp.route("/login", methods=['POST'])
 def login() -> Response:
@@ -153,4 +157,44 @@ def refresh_token() -> Response:
             "message": "Failed to refresh token",
             "data": str(e)
         }), 500
+
+
         
+"""
+    Route for log out
+"""
+@auth_bp.route("/logout", methods=['POST'])
+@token_required
+def logout():
+    session = SessionLocal()
+    current_user = g.current_user
+    try:
+        token = request.headers.get('Authorization')
+        if not token:
+            return api_response(True, "Token is missing", [], 401)
+            
+        token = token.split(" ")[1] if " " in token else token
+        
+        payload: dict = decode_token(token)
+        token_type = payload.get("type", "access")
+        exp_timestamp = payload.get("exp")
+        expires_at = datetime.datetime.utcfromtimestamp(exp_timestamp)
+        
+        '''add to TokenBlacklist'''
+        blacklisted_token = TokenBlacklist(
+            token = token,
+            user_id = str(current_user.id),
+            token_type = token_type,
+            expires_at = expires_at,
+            reason = "logout"
+        )        
+        session.add(blacklisted_token)
+        session.commit()
+        return api_response(False, "User logged out successfully", [], 200)
+    
+    except Exception as e:
+        session.rollback()
+        return api_response(True, "Logout failed", str(e), 500)
+
+    finally:
+        session.close()
