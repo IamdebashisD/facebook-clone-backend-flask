4203e694369852bb344da6fc31b72de9d782d929
IamdebashisD <adebashisdas626@gmail.com>
2025-10-28 19:12:46 +0530

    feat(token): Add TokenBlacklist model for managing blacklisted tokens

---
+++ app/models/token_blacklist_model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, String , Text, ForeignKey, DateTime
+from app.database.db import Base
+import uuid
+from datetime import datetime, timezone
+
+
+class TokenBlacklist(Base):
+    __tablename__ = 'token_blacklist'
+    
+    id: str = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    token: str = Column(Text, nullable=False, unique=True)              # JWT string
+    user_id: str =  Column(String(36), nullable=False)
+    token_type: str = Column(String(10), nullable=False)
+    blacklisted_at: datetime = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    expires_at: datetime = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    reason: str = Column(String(100), default="logout", nullable=True)
+    
+    def __repr__(self) -> str:
+        return f"<TokenBlacklist( user_id={self.user_id}, reason={self.reason}, blacklisted_at={self.blacklisted_at})>"
+    
