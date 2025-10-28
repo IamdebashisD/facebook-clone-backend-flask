763d96a27a96734832ccdbc814bb837879e1ae14
IamdebashisD <adebashisdas626@gmail.com>
2025-10-28 19:10:33 +0530

    fix(like): Update created_at field to use timezone-aware datetime

--- app/models/like_model.py
+++ app/models/like_model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, String, ForeignKey, DateTime
 from app.database.db import Base
 import uuid
-import datetime
+from datetime import datetime, timezone
 
 class Like(Base):
     __tablename__ = "likes"
@@ -9,7 +9,7 @@ class Like(Base):
     id: str = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     post_id: str = Column(String(36), ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
     user_id: str = Column(String(36), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    created_at: datetime.datetime = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    created_at: datetime = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
     
     def __repr__(self) -> str:
         return f"<Like(ID={self.id}, POST_ID={self.post_id} USER_ID={self.user_id})>"
