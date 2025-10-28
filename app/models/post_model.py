2881618a0faaa4fb1bc5be5fc6116111571ffa6d
IamdebashisD <adebashisdas626@gmail.com>
2025-10-28 19:10:46 +0530

    fix(post): Update created_at field to use timezone-aware datetime

--- app/models/post_model.py
+++ app/models/post_model.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from app.database.db import Base
 import uuid
-import datetime
+from datetime import datetime, timezone
 
 
 class Post(Base):
@@ -12,7 +12,7 @@ class Post(Base):
     title: str = Column(String(255), nullable=False)
     content: str = Column(Text, nullable=False)
     user_id: str = Column(String(36), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    created_at: datetime.datetime = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    created_at: datetime = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
     
     comments = relationship("Comment", backref="post", cascade="all, delete-orphan")
     likes = relationship("Like", backref="post", cascade="all, delete-orphan")
