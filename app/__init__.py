c5be55ea461619c352be0661486955a6590e7cfb
IamdebashisD <adebashisdas626@gmail.com>
2025-11-02 00:09:59 +0530

    feat(mail): initialize Flask-Mail and configure app with Config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from .database.db import Base, engine
 from app.routes import all_blueprints
+from flask_mail import Mail
+from app.config import Config
 
 
 # --- FIX START ---
@@ -12,14 +14,20 @@ import app.models.like_model
 import app.models.comment_model
 import app.models.token_blacklist_model
 
+# create a instance of flask mail
+mail = Mail()
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    mail.init_app(app)
     
     Base.metadata.create_all(bind=engine)
     
     '''Register all blueprint here - '''
     for bp in all_blueprints:
         app.register_blueprint(bp)
-
+        
+        
     
     return app
