d9b4f6e8645e0e74901f0743b606042e295ba496
IamdebashisD <adebashisdas626@gmail.com>
2025-11-05 22:53:10 +0530

    feat(routes): include comment routes in the list of blueprints

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,7 @@ from .ping_routes import ping_bp
 from .auth_routes import auth_bp
 from .user_routes import user_bp
 from .post_routes import post_bp
+from .comment_routes import comment_bp
 
-all_blueprints = [ping_bp, auth_bp, user_bp, post_bp]
+all_blueprints = [ping_bp, auth_bp, user_bp, post_bp, comment_bp]
 
