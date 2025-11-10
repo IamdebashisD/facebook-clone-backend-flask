069b883d5399c97d527bfad3cdafc4a3ed7275e1
IamdebashisD <adebashisdas626@gmail.com>
2025-11-10 17:38:20 +0530

    feat(routes): include like routes in the list of blueprints

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,7 @@ from .auth_routes import auth_bp
 from .user_routes import user_bp
 from .post_routes import post_bp
 from .comment_routes import comment_bp
+from .like_route import like_bp
 
-all_blueprints = [ping_bp, auth_bp, user_bp, post_bp, comment_bp]
+all_blueprints = [ping_bp, auth_bp, user_bp, post_bp, comment_bp, like_bp]
 
