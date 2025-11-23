ad8025ff2acd344477c6728c8d2bee1ae7a52b06
IamdebashisD <adebashisdas626@gmail.com>
2025-11-23 20:23:30 +0530

    feat(comment schema): update comment schema for single-level replies

--- app/schemas/comment_schema.py
+++ app/schemas/comment_schema.py
@@ -1,5 +1,29 @@
 from marshmallow import Schema, fields, validate
 
+
+class userMiniSchema(Schema):
+    id = fields.Str(dump_only=True)
+    username = fields.Str(required=True)
+    
+    
+    
+class replySchema(Schema):
+    id = fields.Str(dump_only=True)
+    post_id = fields.Str(required=True)
+    user_id = fields.Str(dump_only=True)
+    user = fields.Nested(userMiniSchema)
+    content = fields.Str(
+        required=True,
+        validate=validate.Length(min=1, max=2000),
+        error_messages={
+            "required": "Content is required",
+        }
+    )
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+    
+    
+    
 class CommentSchema(Schema):
     class Meta:
         ordered = True
@@ -7,6 +31,7 @@ class CommentSchema(Schema):
     id = fields.Str(dump_only=True)
     post_id = fields.Str(required=True)
     user_id = fields.Str(dump_only=True)
+    user = fields.Nested(userMiniSchema)
     
     content = fields.Str(
         required=True,
@@ -15,5 +40,8 @@ class CommentSchema(Schema):
             "required": "Content is required",
         }
     )
+    # List of replies
+    replies = fields.List(fields.Nested(replySchema), dump_only=True)
+    
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
